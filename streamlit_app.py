88cc76feff033c8682bcbb33a67872090e8b3cb0
guna17215 <109857836+guna17215@users.noreply.github.com>
2022-11-26 19:25:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,12 @@ import streamlit as st
 st.title("Hello, My name is Gunavardhan Reddy, learning streamlit!")
 
 st.header("Here we are learning how BUTTTONS work in streamlit.")
+st.subheader("1. Normal Buttons.")
 result = st.button("Button.")
 res = str(result)
-st.write("On Click : " + res)
+st.write("On Click, it becomes true: " + res)
 if st.button("click me"):
 	st.write("Hey there, I am button. :heart:")
 else:
 	st.write("you haven't clicked the button. :point_up:")
+st.subheader("2.Download Buttons.")
