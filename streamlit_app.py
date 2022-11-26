21649dd0ea32c6ece9793d7e001348fa9bc925f1
guna17215 <109857836+guna17215@users.noreply.github.com>
2022-11-26 18:52:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Hello, My name is Gunavardhan Reddy, learning streamlit!")
 
 st.header("Here we are learning how BUTTTONS work in streamlit.")
-result = st.button
+result = st.button()
 st.write(result)
 if st.button("click me"):
 	st.write("Hey there, I am button. :heart:")
