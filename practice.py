badd9f89d1f593c6ec63d64954e60540fc96676a
guna17215 <109857836+guna17215@users.noreply.github.com>
2022-12-02 17:13:09 +0530

    Update practice.py

--- practice.py
+++ practice.py
@@ -3,3 +3,11 @@ import streamlit as st;
 
 st.title("working with pandas and streamit.")
 
+uploaded_file = st.file_uploader("please upload your file here.")
+
+if uploaded_file:
+  df = pd.read_csv(uploaded_file)
+  st.write(df.describe())
+else:
+  st.wtrie("file not uploaded")
+  
